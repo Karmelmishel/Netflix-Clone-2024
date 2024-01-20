0e346c77097216363a5bee355875858016bcb903
Karmelmishel <karmel.mishel@gmail.com>
2024-01-20 15:53:24 +0300

    final-part

---
+++ natflix-clone/src/Components/Rows/Row/Row.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react'
+import axios from '../../utils/axios'
+import './Row.css'
+import movieTrailer from "movie-trailer"
+import YouTube from 'react-youtube'
+const base_url = "https://image.tmdb.org/t/p/original/"
+const Row = ({title, ferchUrl, isLargeRow }) => {
+    const [movies, setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl]= useState('')
+
+    useEffect(() => {
+      async function fetchData() {
+        const request = await axios.get(ferchUrl);
+        // console.log(request)
+        setMovies(request.data.results);
+        return request;
+      }
+      fetchData();
+    }, [ferchUrl]);
+  //   console.log(movies);
+  const handelClick = (movie)=>{
+    if(trailerUrl){
+        setTrailerUrl('')
+    }else{
+        movieTrailer(movie?.title || movie?.orginal_name || movie?.name)
+        .then((url)=>{
+const urlParams = new URLSearchParams(new URL(url).search)
+console.log(url)
+console.log(urlParams)
+setTrailerUrl(urlParams.get('v'))
+        })
+    }
+  }
+  const opts ={
+    height: '390',
+    width: '100%',
+    playerVars: {autoplay: 1,},
+  }
+  
+    return (
+      <div className="row">
+        <h1>{title}</h1>
+        <div className="row__posters">
+          {movies?.map((movie, i) => {
+              let img= (<img onClick={()=>handelClick(movie)} key={i} className={`row__poster ${isLargeRow && 'row__posterLarge'}`}
+              src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
+              
+            />)
+            return img
+          })}
+        </div>
+        <div style={{paddind: '40px'}}>
+            {trailerUrl && <YouTube videoId={trailerUrl} opts={opts}/>}
+        </div>
+      </div>
+    );
+}
+
+export default Row
---
+++ natflix-clone/src/Components/Rows/RowList/RowList.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Row from '../Row/Row'
+import requests from '../../utils/requests'
+const RowList = () => {
+  return (
+    <>
+      <Row title="NETFLEX ORGINALS" ferchUrl={requests.NetflixOrginal} isLargeRow/>
+      <Row title="Trending Now" ferchUrl={requests.Trending}  />
+      <Row title="TV Show" ferchUrl={requests.TVShow} />
+      <Row title="Top Rated" ferchUrl={requests.TopRatedMovies} />
+      <Row title="Action Movie" ferchUrl={requests.ActionMovies} />
+      <Row title="Comedy Movie" ferchUrl={requests.ComedyMovies} />
+      <Row title="Horror Movie" ferchUrl={requests.HorrorMovies} />
+      <Row title="Romance Movie" ferchUrl={requests.RomanceMovies} />
+      <Row title="Documentarie" ferchUrl={requests.Documentaries} />
+      <Row title="Upcoming" ferchUrl={requests.Upcoming} />
+      
+    </>
+  )
+}
+
+export default RowList
